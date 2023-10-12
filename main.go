c007e169dd58fea8b03a1d4a8ab6e01993d50a35
antaralle <antaralle@gmail.com>
2023-10-12 14:32:59 +0300

    123

--- main.go
+++ main.go
@@ -22,43 +22,34 @@ func newCollector() *mCollector {
 			"info about rds connections",
 			[]string{"ClientAddress", "ConnectedResource", "UserName"}, nil,
 		),
-		rds_total_metric: prometheus.NewDesc("rds_total_metric",
-			"total rds connections",
-			nil, nil,
-		),
 	}
 }
 
 func (collector *mCollector) Describe(ch chan<- *prometheus.Desc) {
 
 	ch <- collector.rds_metric
-	ch <- collector.rds_total_metric
+	//ch <- collector.rds_total_metric
 }
 
 func (collector *mCollector) Collect(ch chan<- prometheus.Metric) {
 
 	var metricValue = 1.0
 	var labels []string
+	var n []string
 
 	res := []rdsUserStats{}
 	wmi.QueryNamespace("SELECT ClientAddress, ConnectedResource, UserName FROM Win32_TSGatewayConnection", &res, "root\\cimv2\\TerminalServices")
-	labels = append(labels, res[0].ClientAddress)
-	labels = append(labels, res[0].ConnectedResource)
-	labels = append(labels, res[0].UserName)
+
+	for _, v := range res {
+		labels = append(n, v.ClientAddress, v.ConnectedResource, v.UserName)
+		ch <- prometheus.MustNewConstMetric(collector.rds_metric, prometheus.GaugeValue, metricValue, labels...)
+	}
 
 	// m1 := prometheus.MustNewConstMetric(collector.rds_metric, prometheus.GaugeValue, metricValue, labels...)
 	// m2 := prometheus.MustNewConstMetric(collector.rds_total_metric, prometheus.GaugeValue, float64(len(res)))
 
-	ch <- prometheus.MustNewConstMetric(collector.rds_metric, prometheus.GaugeValue, metricValue, labels...)
-	ch <- prometheus.MustNewConstMetric(collector.rds_total_metric, prometheus.GaugeValue, float64(len(res)))
-	var l2 []string
-	l2 = append(labels, res[1].ClientAddress)
-	l2 = append(labels, res[1].ConnectedResource)
-	l2 = append(labels, res[1].UserName)
-	ch <- prometheus.MustNewConstMetric(collector.rds_metric, prometheus.GaugeValue, metricValue, l2...)
+	//ch <- prometheus.MustNewConstMetric(collector.rds_total_metric, prometheus.GaugeValue, float64(len(res)))
 
-	//ch <- prometheus.MustNewConstMetric(collector.rds_metric, prometheus.GaugeValue, metricValue, labels...)
-	ch <- prometheus.MustNewConstMetric(collector.rds_total_metric, prometheus.GaugeValue, float64(len(res)))
 }
 
 type rdsUserStats struct {
@@ -84,6 +75,7 @@ func getRdsStatistics() RdsStats {
 }
 func main() {
 	c := newCollector()
+	prometheus.Unregister(prometheus.NewGoCollector())
 	prometheus.MustRegister(c)
 
 	http.Handle("/metrics", promhttp.Handler())
